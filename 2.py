c3ef37fb8b4192681ab5c68a23a0f63e62fb6641
UsLyX <109165881+UsLyX@users.noreply.github.com>
2024-09-26 10:43:01 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,6 @@
 import math
-e=1.5
 x=3.981*10**(-2)
 y=-1.625*10**3
 z=0.512
-s=2**(-x)*math.sqrt(x+math.sqrt(math.sqrt(abs(y))))*math.cbrt(e**((x-1)/math.sin(z)))
+s=2**(-x)*math.sqrt(x+math.sqrt(math.sqrt(abs(y))))*math.cbrt(math.e**((x-1)/math.sin(z)))
 print(s)
