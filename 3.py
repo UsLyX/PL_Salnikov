a30ae3186dc583cf132baf27bd1b2fc2e865a41e
UsLyX <109165881+UsLyX@users.noreply.github.com>
2024-10-04 12:34:33 +0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,8 @@
+def sum(a, b, c)
+ return (a + b + c)
+
+a = int(input('Введите 1-е число: '))
+b = int(input('Введите 2-е число: '))
+c = int(input('Введите 3-е число: '))
+
+print(f'Сумма {a} + {b} + {c} равна: {sum(a, b, c)}')
